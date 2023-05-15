91537f1a83e624c2a859ff1177c6e3c2997dbf31
NagarajHalayyanavaramath <nagarajhalayyanavaramath@gmail.com>
2023-05-15 20:25:42 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,52 @@
+const navbtn = document.querySelector("#nav-btn");
+const navbar = document.querySelector("#navbar");
+
+navbtn.addEventListener("click", function () {
+  navbar.classList.toggle("nav-open");
+});
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
+//sticky navigation
+const sectionMainEl = document.querySelector(".main--section");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionMainEl);
